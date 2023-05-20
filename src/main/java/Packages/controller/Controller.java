c2b4bf8d6019ca4da82895071d07045e2e2c101a
rocketpowers <rocketpowerswillian@gmail.com>
2023-05-20 10:12:20 -0300

    crud works

--- src/main/java/Packages/controller/Controller.java
+++ src/main/java/Packages/controller/Controller.java
@@ -1,15 +1,56 @@
 package Packages.controller;
 
+import javax.swing.JOptionPane;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import Packages.model.Client;
+import Packages.repository.ClientRepository;
 
+@RestController
 public class Controller {
 
-	@GetMapping("/test")
-	public String test() {
-		return "hello word";
+	@Autowired
+	private ClientRepository repository;
+	
+	@PostMapping("/register")
+	public Client register(@RequestBody Client savClient) {
+		
+		System.out.println("user save sucessfull");
+		
+		//JOptionPane.showMessageDialog(null, "user save sucessfull");
+		
+		
+		return repository.save(savClient) ;
+		
+	}
+
+	@GetMapping("/list")
+	public Iterable<Client> list() {
+		return repository.findAll();
 
 	}
+	
+	@PutMapping("/edit")
+	public Client edit(@RequestBody Client editClient) {
+		return repository.save(editClient);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void del(@PathVariable long id) {
+		repository.deleteById(id);
+		System.out.println("user removed sucessfull");
+		//JOptionPane.showInputDialog("user removed sucessfull");
+	}
+	
+	
+
+	
 }
--- src/main/java/Packages/repository/ClientRepository.java
+++ src/main/java/Packages/repository/ClientRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import Packages.model.Client;
 
-public interface Repository  extends JpaRepository<Client, Long>{
-	
-	
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
