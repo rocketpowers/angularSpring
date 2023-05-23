8a9f336c0e760e972b18d1d5fcb2c868105b092d
rocketpowers <rocketpowerswillian@gmail.com>
2023-05-22 21:04:07 -0300

    rectified

--- frontend/api-front/src/app/app.component.spec.ts
+++ frontend/api-front/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'api-front'`, () => {
+  it(`should have as title 'api-font'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('api-front');
+    expect(app.title).toEqual('api-font');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('api-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('api-font app is running!');
   });
 });
